8676e2b70f68d636acd4acfbaa55cd7b36185c00
Bethel Eyo <betheleyo@gmail.com>
2024-06-23 18:09:50 +0100

    Add listbucketPolicy to adminRole
    Also attach the adminRole to the adminsGroup using the roleArn

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -6,7 +6,7 @@ import {
   UserPool,
   UserPoolClient,
 } from "aws-cdk-lib/aws-cognito";
-import { FederatedPrincipal, Role } from "aws-cdk-lib/aws-iam";
+import { Effect, FederatedPrincipal, PolicyStatement, Role } from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 
 export class AuthStack extends Stack {
@@ -22,10 +22,10 @@ export class AuthStack extends Stack {
 
     this.createUserPool();
     this.createUserPoolClient();
-    this.createAdminsGroup();
     this.createIdentityPool();
     this.createRoles();
     this.attachRoles();
+    this.createAdminsGroup();
   }
 
   private createUserPool() {
@@ -61,6 +61,7 @@ export class AuthStack extends Stack {
     new CfnUserPoolGroup(this, "SpacesAdmins", {
       userPoolId: this.userPool.userPoolId,
       groupName: "admins",
+      roleArn: this.adminRole.roleArn,
     });
   }
 
@@ -125,6 +126,13 @@ export class AuthStack extends Stack {
         "sts:AssumeRoleWithWebIdentity"
       ),
     });
+    this.adminRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["s3:ListAllMyBuckets"],
+        resources: ["*"],
+      })
+    );
   }
 
   private attachRoles() {
