975c0478d49851a795e4405533033d5d795505f9
Bethel Eyo <betheleyo@gmail.com>
2024-06-23 06:39:50 +0100

    Create IdentityPool
    This will enable us  to generate temporary credentials for the client side to use to perform aws sdk operations such as pushing files to s3

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -1,10 +1,16 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
-import { CfnUserPoolGroup, UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito";
+import {
+  CfnIdentityPool,
+  CfnUserPoolGroup,
+  UserPool,
+  UserPoolClient,
+} from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 
 export class AuthStack extends Stack {
   public userPool!: UserPool;
   private userPoolClient!: UserPoolClient;
+  private identityPool!: CfnIdentityPool;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -12,6 +18,7 @@ export class AuthStack extends Stack {
     this.createUserPool();
     this.createUserPoolClient();
     this.createAdminsGroup();
+    this.createIdentityPool();
   }
 
   private createUserPool() {
@@ -39,14 +46,30 @@ export class AuthStack extends Stack {
     });
 
     new CfnOutput(this, "SpaceUserPoolClientId", {
-        value: this.userPoolClient.userPoolClientId,
-      });
+      value: this.userPoolClient.userPoolClientId,
+    });
   }
 
-  private createAdminsGroup(){
-    new CfnUserPoolGroup(this, 'SpacesAdmins', {
-        userPoolId: this.userPool.userPoolId,
-        groupName: 'admins'
-    })
+  private createAdminsGroup() {
+    new CfnUserPoolGroup(this, "SpacesAdmins", {
+      userPoolId: this.userPool.userPoolId,
+      groupName: "admins",
+    });
+  }
+
+  private createIdentityPool() {
+    this.identityPool = new CfnIdentityPool(this, "SpacesIdentityPool", {
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: this.userPoolClient.userPoolClientId,
+          providerName: this.userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    new CfnOutput(this, "SpaceIdentityPoolId", {
+      value: this.identityPool.ref,
+    });
   }
 }
