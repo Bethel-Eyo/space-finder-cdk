fee064337875d29da654e346aa3da8aaea233911
Bethel Eyo <betheleyo@gmail.com>
2024-06-27 21:33:39 +0100

    Create photos bucket and add cors permission

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,10 +1,13 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { AttributeType, Table as DynamodbTable, ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Bucket, HttpMethods, IBucket, ObjectOwnership } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { getSuffixFromStack } from "../Utils";
 
 export class DataStack extends Stack {
   public readonly spacesTable: ITable;
+  public readonly photosBucket: IBucket;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -17,5 +20,30 @@ export class DataStack extends Stack {
       },
       tableName: `SpacesTable-${suffix}`, // this should be dynamic to avoid tables using same names
     });
+
+    this.photosBucket = new Bucket(this, 'SpaceFinderPhotos', {
+      bucketName: `space-finder-photos-${suffix}`,
+      cors: [{
+        allowedMethods: [
+          HttpMethods.HEAD,
+          HttpMethods.GET,
+          HttpMethods.PUT
+        ],
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"]
+      }],
+      objectOwnership: ObjectOwnership.OBJECT_WRITER,
+      // accessControl: BucketAccessControl.PUBLIC_READ // said to currently not be working
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      }
+    });
+
+    new CfnOutput(this, "SpaceFinderPhotosBucketName", {
+      value: this.photosBucket.bucketName
+    })
   }
 }
